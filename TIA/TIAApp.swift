45443ec507d65566838bc9e5b7fe50b62f10183f
serhii.lomov <serhii.lomov23@gmail.com>
2022-04-06 01:01:33 +0300

    Initial Commit

---
+++ TIA/TIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TIAApp.swift
+//  TIA
+//
+//  Created by Serhii.Lomov on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
